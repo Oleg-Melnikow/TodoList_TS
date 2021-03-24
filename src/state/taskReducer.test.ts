fb89e908225bbbff6bae92c74b8b49c8032ac5b2
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2021-03-24 22:09:18 +0300

    add test for add task and change task status

--- src/state/taskReducer.test.ts
+++ src/state/taskReducer.test.ts
@@ -1,5 +1,5 @@
 import {TaskStateType} from "../App";
-import {removeTaskAC, taskReducer} from "./taskReducer";
+import {addTaskAC, removeTaskAC, taskReducer, changeTaskStatusAC} from "./taskReducer";
 
 test('correct task should be deleted from array', () => {
 
@@ -16,10 +16,57 @@ test('correct task should be deleted from array', () => {
         ]
     }
 
-    const action = removeTaskAC("2", "todoListID_2");
+    const action = removeTaskAC("todoListID_2", "2");
     const endState = taskReducer(startState, action)
 
     expect(endState["todoListID_1"].length).toBe(3);
     expect(endState["todoListID_2"].length).toBe(2);
     expect(endState["todoListID_2"].every(t => t.id !== "2")).toBeTruthy();
+});
+
+test('correct task should be added from array', () => {
+
+    const startState: TaskStateType = {
+        "todoListID_1": [
+            {id: "1", isDone: true, title: "HTML"},
+            {id: "2", isDone: true, title: "JS"},
+            {id: "3", isDone: false, title: "React"},
+        ],
+        "todoListID_2": [
+            {id: "1", isDone: true, title: "bread"},
+            {id: "2", isDone: false, title: "milk"},
+            {id: "3", isDone: false, title: "butter"},
+        ]
+    }
+
+    const action = addTaskAC("todoListID_2", "juice");
+    const endState = taskReducer(startState, action)
+
+    expect(endState["todoListID_1"].length).toBe(3);
+    expect(endState["todoListID_2"].length).toBe(4);
+    expect(endState["todoListID_2"][0].id).toBeDefined();
+    expect(endState["todoListID_2"][0].title).toBe("juice");
+    expect(endState["todoListID_2"][0].isDone).toBe(false);
+});
+
+test('status of specified task should be changed', () => {
+
+    const startState: TaskStateType = {
+        "todoListID_1": [
+            {id: "1", isDone: true, title: "HTML"},
+            {id: "2", isDone: true, title: "JS"},
+            {id: "3", isDone: false, title: "React"},
+        ],
+        "todoListID_2": [
+            {id: "1", isDone: true, title: "bread"},
+            {id: "2", isDone: true, title: "milk"},
+            {id: "3", isDone: false, title: "butter"},
+        ]
+    }
+
+    const action = changeTaskStatusAC("todoListID_2", "2", false);
+    const endState = taskReducer(startState, action)
+
+    expect(endState["todoListID_2"][1].isDone).toBeFalsy();
+    expect(endState["todoListID_1"][1].isDone).toBeTruthy();
 });
