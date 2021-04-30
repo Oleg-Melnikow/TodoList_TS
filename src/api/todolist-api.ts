706dbc018f79c1d8fcaf262d86fef32668ba60fb
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2021-04-30 17:49:55 +0300

    add type for response

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -8,21 +8,35 @@ const settings = {
 }
 
 const instance = axios.create({
-    baseURL: "https://social-network.samuraijs.com/api/1.1/todo-lists/",
+    baseURL: "https://social-network.samuraijs.com/api/1.1/",
     ...settings
 })
 
+type TodolistType = {
+    id: string
+    addedDate: string
+    order: number
+    title: string
+}
+
+type ResponseType<T = {}> = {
+    resultCode: number
+    messages: Array<string>,
+    fieldsErrors: Array<string>
+    data: T
+}
+
 export const todolistAPI = {
-    createTodoList(){
-        return instance.post("",{title: "newJS"})
+    createTodoList(title: string){
+        return instance.post<ResponseType<TodolistType>>("todo-lists/",{title})
     },
     getTodoLists(){
-        return instance.get("")
+        return instance.get<TodolistType[]>("todo-lists/")
     },
     updateTodolist(todolistId: string, title: string) {
-        return instance.put(`${todolistId}`, {title: title})
+        return instance.put<ResponseType>(`todo-lists/${todolistId}`, {title: title})
     },
     deleteTodoList(todolistId: string){
-        return  instance.delete(`${todolistId}`)
+        return  instance.delete<ResponseType>(`todo-lists/${todolistId}`)
     }
 }
