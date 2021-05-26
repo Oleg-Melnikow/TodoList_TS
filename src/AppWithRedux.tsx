b8754fa1bc4fcaef035eb8d3c2181bdbbf944e8a
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2021-05-26 17:06:48 +0300

    add routes

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -1,73 +1,19 @@
-import React, {useCallback, useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import {TodoList} from "./Todolist";
-import {AddItemForm} from "./components/AddItemForm/AddItemForm";
-import {AppBar, Button, Container, Grid, IconButton, LinearProgress, Paper, Typography} from "@material-ui/core";
+import {AppBar, Button, Container, Grid, IconButton, LinearProgress, Typography} from "@material-ui/core";
 import Toolbar from '@material-ui/core/Toolbar';
 import {Menu} from "@material-ui/icons";
-import {
-    changeTodoListFilterAC,
-    TodoListDomainType,
-    FilterValueType,
-    setTodoListsTC,
-    updateTodoListTitleTC,
-    createTodoListsTC,
-    deleteTodoListsTC
-} from "./state/todoListReducer";
-import {
-    deleteTasksTC,
-    addTaskTC, updateTaskTC, TaskStateType
-} from "./state/taskReducer";
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {AppRootStateType} from "./state/store";
-import {TaskStatuses} from "./api/todolist-api";
 import {RequestStatusType} from "./state/appReducer";
 import {ErrorSnackbar} from "./components/ErrorSnackbar/ErrorSnackbar";
+import {TodoListContainer} from "./TodolistContainer";
+import {Redirect, Route, Switch } from 'react-router-dom';
+import {Login} from "./features/Login";
 
 export function AppWithRedux() {
 
-    useEffect(() => {
-        dispatch(setTodoListsTC())
-    }, []);
-
     const status = useSelector<AppRootStateType, RequestStatusType>( state => state.app.status)
-
-    const todoLists = useSelector<AppRootStateType, Array<TodoListDomainType>>(state => state.todoLists);
-    const tasks = useSelector<AppRootStateType, TaskStateType>(state => state.tasks);
-    const dispatch = useDispatch();
-
-    const remoteTask = useCallback((todoListId: string, taskId: string) => {
-        dispatch(deleteTasksTC(todoListId, taskId))
-    }, [dispatch])
-
-    const addTask = useCallback((title: string, todoListId: string) => {
-        dispatch(addTaskTC(todoListId, title))
-    }, [dispatch])
-
-    const changeStatus = useCallback((taskId: string, status: TaskStatuses, todoListId: string) => {
-        dispatch(updateTaskTC(taskId, {status}, todoListId))
-    }, [dispatch])
-
-    const changeTaskTitle = useCallback((taskId: string, newTitle: string, todoListId: string) => {
-        dispatch(updateTaskTC(taskId, {title: newTitle}, todoListId))
-    }, [dispatch])
-
-    const addTodoList = useCallback((title: string) => {
-        dispatch(createTodoListsTC(title));
-    }, [dispatch])
-
-    const remoteTodoList = useCallback((todoListId: string) => {
-        dispatch(deleteTodoListsTC(todoListId));
-    }, [dispatch])
-
-
-    const changeFilter = useCallback((value: FilterValueType, todoListId: string) => {
-        dispatch(changeTodoListFilterAC(todoListId, value));
-    }, [dispatch])
-
-    const changeTodoListTitle = useCallback((id: string, newTitle: string) => {
-        dispatch(updateTodoListTitleTC(id, newTitle))
-    }, [dispatch])
 
     return (
         <div className="App">
@@ -82,28 +28,15 @@ export function AppWithRedux() {
                     </Typography>
                     <Button color="inherit">Login</Button>
                 </Toolbar>
+                {status === "loading" && <LinearProgress color="secondary"/>}
             </AppBar>
-            {status === "loading" && <LinearProgress color="secondary"/>}
             <Container fixed>
-                <Grid container style={{padding: "15px"}}>
-                    <AddItemForm addItem={addTodoList}/>
-                </Grid>
-                <Grid container spacing={6}>
-                    {todoLists.map(tl => {
-                        return <Grid item key={tl.id}>
-                            <Paper style={{padding: "10px"}}>
-                                <TodoList key={tl.id} title={tl.title} tasks={tasks[tl.id]}
-                                          remoteTask={remoteTask} id={tl.id}
-                                          changeFilter={changeFilter} addTask={addTask}
-                                          changeStatus={changeStatus} filter={tl.filter}
-                                          changeTaskTitle={changeTaskTitle}
-                                          entityStatus={tl.entityStatus}
-                                          changeTodoListTitle={changeTodoListTitle}
-                                          remoteTodoList={remoteTodoList}/>
-                            </Paper>
-                        </Grid>
-                    })}
-                </Grid>
+                <Switch>
+                    <Route exact path={'/'} render={() => <TodoListContainer/>}/>
+                    <Route path={'/login'} render={() => <Login/>}/>
+                    <Route path={'/404'} render={() => <h1>404: PAGE NOT FOUND</h1>}/>
+                    <Redirect from={"*"} to={"/404"}/>
+                </Switch>
             </Container>
         </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import { AppWithRedux } from './AppWithRedux';
-import { store } from './state/store';
+import {Provider} from 'react-redux';
+import {AppWithRedux} from './AppWithRedux';
+import {store} from './state/store';
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
     <Provider store={store}>
-        <AppWithRedux/>
+        <BrowserRouter>
+            <AppWithRedux/>
+        </BrowserRouter>
     </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
